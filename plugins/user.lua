34380a69e2a6a85c312197fc6957e16642116581
jianglong <jianglong199212@gmail.com>
2023-06-18 20:29:17 +0800

    update

--- plugins/user.lua
+++ plugins/user.lua
@@ -83,4 +83,17 @@ return {
         },
     },
 
+    {
+        'crusj/bookmarks.nvim',
+        keys = {
+            { "<tab><tab>", mode = { "n" } },
+        },
+        branch = 'main',
+        dependencies = { 'nvim-web-devicons' },
+        config = function()
+            require("bookmarks").setup()
+            require("telescope").load_extension("bookmarks")
+        end
+    }
+
 }
