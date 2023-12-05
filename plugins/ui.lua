f962672ea8ad79e8b273b4514824dd9d1f685af7
jianglong <jianglong199212@gmail.com>
2023-12-05 11:29:41 +0800

    update

--- plugins/ui.lua
+++ plugins/ui.lua
@@ -1,8 +1,8 @@
-vim.g.neovide_remember_window_size = true
-vim.g.neovide_transparency = 0.95
-vim.g.neovide_cursor_vfx_mode = "sonicboom"
-
-
+if vim.g.neovide then
+    vim.g.neovide_remember_window_size = true
+    vim.g.neovide_transparency = 1.0
+    vim.g.neovide_cursor_vfx_mode = "sonicboom"
+end
 
 return {
     {
