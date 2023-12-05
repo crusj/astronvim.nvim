f4f258ad05d32e56405e3347b319bf89dd5d29e7
jianglong <jianglong199212@gmail.com>
2023-12-05 11:15:32 +0800

    update

--- plugins/ui.lua
+++ plugins/ui.lua
@@ -1,4 +1,8 @@
 vim.g.neovide_remember_window_size = true
+vim.g.neovide_transparency = 0.95
+vim.g.neovide_cursor_vfx_mode = "sonicboom"
+
+
 
 return {
     {
--- plugins/user.lua
+++ plugins/user.lua
@@ -56,7 +56,7 @@ return {
         event = 'InsertEnter',
         config = function()
             -- Change '<C-g>' here to any keycode you like.
-            vim.keymap.set('i', '<c-o>', function() return vim.fn['codeium#Accept']() end, { expr = true })
+            vim.keymap.set('i', '<c-o>', function() return vim.fn['codeium#Accept']() end, { expr = true,silent = true })
             vim.keymap.set('i', '<c-j>', function() return vim.fn['codeium#CycleCompletions'](1) end, { expr = true })
             vim.keymap.set('i', '<c-k>', function() return vim.fn['codeium#CycleCompletions'](-1) end, { expr = true })
             vim.keymap.set('i', '<c-x>', function() return vim.fn['codeium#Clear']() end, { expr = true })
